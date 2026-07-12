b3a8aed9bab9bd6a9fc721549d615573fb3d88f8
Multi <akak.nacereddine@gmail.com>
2026-07-12 14:08:04 +0100

    Route!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
 import './App.css'
 import Numberbtn from './Numberbtn.jsx'
+import {Routes, Route} from "react-router-dom";
 export default function App() {
   return (
+    <div>
+    <Routes>
+      <Route path="/calculator" element={<Numberbtn/>}/>
+    </Routes>
     <div className="App">
       <Numberbtn/>
+    </div>
     </div>
   )
 }
